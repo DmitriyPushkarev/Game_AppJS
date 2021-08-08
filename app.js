1ec6323880a51f93fde1a1c5f289350a8cec52da
Dmitrii <dpushk@gmail.com>
2021-08-08 14:42:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const startBtn = document.querySelector ('#start')
 const screens = document.querySelectorAll ('.screen')
 const timeList = document.querySelector ('#time-list')
 const timeEl = document.querySelector ('#time')
+const board = document.querySelector ('#board')
 let time = 0
 
 startBtn.addEventListener ('click', (event) => {
@@ -19,17 +20,45 @@ timeList.addEventListener ('click', event => {
 
 function startGame () {
     setInterval(decreaseTime, 1000)
+    createRandomCircle ()
     setTime (time)
 }
 
 function decreaseTime () {
-    let current = --time
-    if (current < 10) {
+    if (time === 0) {
+        finishGame ()
+    } else if {
+        let current = --time
+        if (current < 10) {
         current = `0${current}`
     }
-    setTime (current)
+        setTime (current)
+    }
 }
 
 function setTime (value) {
     timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame () {
+
+}
+
+function createRandomCircle () {
+    const circle = doc.createElement ('div')
+    const size = getRandomNumber (10, 60)
+    const qqq = board.getBoundingClientRect()
+    const x = getBoundingClientRect (0, width-size)
+    const y = getBoundingClientRect (0, height-size)
+    circle.classList.add ('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
+
+    board.append (circle)
+}
+
+function getRandomNumber (min, max) {
+    return Math.round(Math.random() * (max-min) + min)
 }
