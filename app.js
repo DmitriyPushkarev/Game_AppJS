dbb35817ffbc4098ec2f324c0685d3e2825fc4d1
Dmitrii <dpushk@gmail.com>
2021-08-08 14:13:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const startBtn = document.querySelector ('#start')
 const screens = document.querySelectorAll ('.screen')
 const timeList = document.querySelector ('#time-list')
+const timeEl = document.querySelector ('#time')
 let time = 0
 
 startBtn.addEventListener ('click', (event) => {
@@ -11,10 +12,24 @@ startBtn.addEventListener ('click', (event) => {
 timeList.addEventListener ('click', event => {
     if (event.target.classList.contains ('time-btn')) {
         time = parseInt (event.target.getAttribute ('data-time'))
+        screens[1].classList.add('up')
         startGame ()
     }
 })
 
 function startGame () {
-    screens[1].classList.add('up')
+    setInterval(decreaseTime, 1000)
+    setTime (time)
+}
+
+function decreaseTime () {
+    let current = --time
+    if (current < 10) {
+        current = `0${current}`
+    }
+    setTime (current)
+}
+
+function setTime (value) {
+    timeEl.innerHTML = `00:${value}`
 }
